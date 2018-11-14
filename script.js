1709e8e41cc11ca222cd2c1f606727139c98d203
Timothy Guertin <guertint@msu.edu>
2018-11-14 18:47:52 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,8 @@
+loadPerpendicularImage = function(){
+    var image = getElementById('parkingImage')
+    if(image!=null){
+        image.src="welcome-to-javascript-where-the-objects-are-made-up-and-13411868.png"
+    }
+}
+
+loadPerpendicularImage()
