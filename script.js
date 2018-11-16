18e6641f8020ca0390e99031634412de023925ed
Timothy Guertin <guertint@msu.edu>
2018-11-15 21:45:19 -0500

    updated js and html

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 loadPerpendicularImage = function(){
-    var image = document.getElementById('parkingImage');
+    var image = document.getElementById('perpendicularImage');
     if(image!=null){
         image.src="welcome-to-javascript-where-the-objects-are-made-up-and-13411868.png"
     }
 };
 
-//loadPerpendicularImage();
+loadParallelImage = function(){
+    var image = document.getElementById('parallelImage');
+    if(image!=null){
+        image.src="welcome-to-javascript-where-the-objects-are-made-up-and-13411868.png"
+    }
+}
+
+
+playRearCameraFeed = function(){
+
+}
+
+playFrontCameraFeed = function(){
+    
+}
