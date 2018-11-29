5288b74afb15f37ff66bbac151de550dda6e7f49
Timothy Guertin <guertint@msu.edu>
2018-11-29 12:05:01 -0500

    added logic for system failure timeouts'

--- script.js
+++ script.js
@@ -46,12 +46,3 @@ playPerpendicularFeed = function(){
         video.play();
     }
 }
-
-drawSteeringBrakeLine = function(){
-    var wheel = document.getElementById("steeringWheel");
-    var wheelcontext =wheel.getContext("2d")
-    wheel.beginPath();
-    wheel.moveTo(0,0);
-    wheel.lineTo(300,150);
-    wheel.stroke();
-}
